284139604e584f3ae8bbe12292453ff9ab6fbe30
Malini <legacysys1@gmail.com>
2026-04-18 09:38:17 +0530

    demo.py first commit

---
+++ demo.py
@@ -0,0 +1,11 @@
+from selenium.webdriver import Chrome
+from time import sleep
+driver = Chrome()
+driver.maximize_window()
+
+driver.get("https://www.w3schools.com/")
+sleep(3)
+a=driver.find_elements("tag name", "h1")
+print(len(a))
+for i in a:
+    print(i.text)
